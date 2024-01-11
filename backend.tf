2dd4ff11eb3861c023a31fce8b83c74b9cfcc2cf
Sushmatech <150417347+Sushmatech@users.noreply.github.com>
2024-01-11 17:20:37 +0530

    updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "naresh-aws-test"
+    bucket = "sush-bucket"
     key = "terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "DynamoDbLock"
   }
 }
