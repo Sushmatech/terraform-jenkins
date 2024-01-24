79e9c098c6839fbfab16c8c81f7db5bafe68c0d5
Sushma Tene <150417347+Sushmatech@users.noreply.github.com>
2024-01-24 15:37:45 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-1" 
+       default     = "ap-south-1" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "test" 
+    default     =  "jenkins" 
 }
 
 variable "instance_type" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0005e0cfe09cc9050" 
+    default     = "ami-00952f27cf14db9cd" 
 }
 variable "versioning" {
     type        = bool
@@ -39,7 +39,7 @@ variable "acl" {
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-s3bucket-"
+    default     = "my-s3bucket-sush"
 }
 variable "tags" {
     type        = map
